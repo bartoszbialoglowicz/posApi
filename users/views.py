881cf3860e4c1e2a41c44aaee5611b8e750d79bc
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-08 01:01:56 +0200

    feat: Created user create view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users import serializers
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
