0b4f4a15d3621dbb4a3a56a2a33fb8f7b8e92ed0
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-08 00:58:21 +0200

    feat: Added products url

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+from api import views
+
+app_name = 'api'
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('products', views.ProductViewSet.as_view(), name='products'),
 ]
