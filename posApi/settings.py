b8ce9c0ebc2e6c1ed9164146c51541c31bfb7a6a
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-08 01:00:29 +0200

    feat: Custom user model set up

--- posApi/settings.py
+++ posApi/settings.py
@@ -126,10 +126,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
