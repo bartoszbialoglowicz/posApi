7778fc0ce0aed2f2c092bd81ff9e65af262c2732
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-08 00:57:29 +0200

    feat: Created basic Manufacturer and Product models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    country = models.CharField(max_length=80)
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    price = models.IntegerField()
