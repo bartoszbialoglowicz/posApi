3d6fa2388513240a7d806ff2d30ab56b72f24ff0
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-04-13 20:15:42 +0200

    feat: Added fk in manufacturer model

--- api/models.py
+++ api/models.py
@@ -5,6 +5,8 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=128, unique=True)
     country = models.CharField(max_length=80)
 
+
 class Product(models.Model):
     name = models.CharField(max_length=128, unique=True)
     price = models.IntegerField()
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
